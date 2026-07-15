b9f71a966c4e5e9f595161f8f6bcf330d1071391
mikehoe <luboskohut.shop@gmail.com>
2026-07-15 20:02:04 +0200

    Implement match-case instead of if-elif-else in main.py

--- main.py
+++ main.py
@@ -14,25 +14,26 @@ task_service = TaskService()
 
 while True:
     show_menu()
-    try:
-        choice = int(input("Enter your choice: "))
-        if choice == 1:
+
+    choice = input("Enter your choice: ").strip()
+    match choice:
+        case "1":
             task_title = input("Enter the task title: ")
             task_service.create_task(task_title)
             print("Task added.")
-        elif choice == 2:
+        case "2":
             tasks = task_service.list_tasks()
             if not tasks:
                 print("No tasks found.")
             else:
                 for index, task in enumerate(tasks, start=1):
                     print(f"{index}. {task}")
-        elif 3 <= choice <= 4:
+        case "3":
             print(f"Your choice: {choice}")
-        elif choice == 5:
+        case "4":
+            print(f"Your choice: {choice}")
+        case '5':
             print("Goodbye!")
             break
-        else:
-            print("Invalid option.")
-    except ValueError:
-        print("Invalid option.")
+        case _:
+            print("Invalid option! Please enter a number between 1 and 5.")
